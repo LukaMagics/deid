9733eae8c1ce15112ed3abe12aba5411d211789c
Myung Suk Oh <opro12266@gmail.com>
2023-08-23 14:27:58 +0900

    error fix

--- docker/fastapi_deid.py
+++ docker/fastapi_deid.py
@@ -29,6 +29,7 @@ async def masking(file: UploadFile = File(...), code: str = Form(...)):
 
     # Set Method
     METHOD_POLICY = set_method_by_code(code)
+    print(METHOD_POLICY)
     
     
     # Apply Deidentification
@@ -41,6 +42,7 @@ async def masking(file: UploadFile = File(...), code: str = Form(...)):
                 begin = PNE['begin']
                 end = PNE['end']
                 label = PNE['label']
+                print(label)
                 method = METHOD_POLICY[label]
                 
                 if "new_form" in sentence.keys():
